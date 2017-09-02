5455768835ddd4824f376de6786bc1b97f098b9f
Ryan <push1235@hotmail.com>
2017-09-01 20:54:36 -0400

    saving progres

--- httpsverifier.go
+++ httpsverifier.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"crypto/sha1"
+	"encoding/base64"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/pushc6/httpsverifier/page"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Checking your certificate fingerprints %s \n\n", r.URL.Path[1:])
+	domainRequested := "facebook.com"
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://facebook.com", nil)
+	resp, _ := client.Do(req)
+
+	for _, val := range resp.TLS.PeerCertificates {
+		for _, dnsName := range val.DNSNames {
+			if strings.ToLower(strings.TrimSpace(dnsName)) == strings.ToLower(domainRequested) {
+				//if base64.StdEncoding.EncodeToString(sha1.Sum(val.Raw)) == "whatweexpect" {
+
+				}
+			}
+		}
+		fmt.Fprintf(w, "Certificate name %s  value is: %x \n\n\n", val.DNSNames, sha1.Sum(val.Raw))
+	}
 }
 
 func verifyHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,9 +44,17 @@ func verifyHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/verify", verifyHandler)
-	http.ListenAndServe(":8080", nil)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Press 1 for server mode, or 2 for client mode: ")
+	text, _ := reader.ReadString('\n')
+	if "1" == strings.TrimSpace(text) {
+		http.HandleFunc("/", handler)
+		http.HandleFunc("/verify", verifyHandler)
+		http.ListenAndServe(":8080", nil)
+	} else {
+		//Load server that just shows status of pre-set URLs and\or files giving ability to add new
+		//and allow them to do one-offs without adding
+	}
 }
 
 func loadPage(title string) (*page.Page, error) {
